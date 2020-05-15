4fb9206684cf9b7a1b674d52af9eb2a8aa60796a
Thanh <thanhnc1997@gmail.com>
2020-05-15 08:46:46 +0700

    9h 15/05/2020

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
     $(".main-nav__overlay").fadeIn();
     setTimeout(function() {
       $(".main-nav").addClass("show");
-    }, 1000);
+    }, 300);
   });
   $("#nav_close").click(function() {
     $(".main-nav").removeClass("show");
     setTimeout(function() {
       $(".main-nav__overlay").fadeOut();
-    }, 500);
-  })
+    }, 300);
+  });
+  $(".location-list__item>.show-location").click(function() {
+    $(this).parent().parent().find(".location-detail").slideDown();
+  });
+  $(".location-list__item>.hide-location").click(function() {
+    $(this).parent().parent().slideUp();
+  });
 })
